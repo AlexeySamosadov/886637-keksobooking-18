dea7c366c4ab46c863861c4ebefec1f88ebe7afc
AlexeySamosadov <asamosadov@gmail.com>
2019-11-06 01:51:51 +0300

    h

--- js/data.js
+++ js/data.js
@@ -10,10 +10,9 @@
     'Апартамент с дизайнерским ремонтом рядом с метро. В комнате двуспальная кровать с ортопедическим матрасом, гардероб, туалетный столик, стул. Техника новая - кондиционер, телевизор, холодильник, СВЧ-печь (микроволновая), электрический чайник, утюг, гладильная доска, встроенный фен. Чайный набор, посуда. Многофункциональная душевая кабина с гидромассажем, туалетные принадлежности (жидкое мыло, гель для душа, шампунь и кондиционер). Комплект постельного белья и полотенец. Бесплатный Wi-Fi.',
     'Стильная, современная и очень теплая квартира рядом с Киевским вокзалом. Дом находится во дворе, что обеспечивает тишину. Квартира - студия, в ней есть всё необходимое для комфортного проживания.'
   ];
-  var featuresTemplate = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
   var CARD_TITLES = ['Новая квартира около метро', 'Ретро квартира', 'Современная квартира Аригато', 'Уютная комната в центре Токио', 'Новый евроремонт', 'Отель с видом на Башню', 'Комната у парка', 'Эксклюзивный Пент-Хаус'];
   var FLAT_TYPE = ['palace', 'flat', 'house', 'bungalo'];
-
+  var featuresTemplate = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
   var generateCheckInOutTime = function () {
     var checkInTimes = ['12:00', '13:00', '14:00'];
     return checkInTimes[randomNumber(2)];
@@ -50,38 +49,40 @@
     }
   };
 
-  window.data = {
-    generateArray: function () {
-      var arr = [];
+  var generateArray = function () {
+    var arr = [];
 
-      for (var i = 0; i < 8; i++) {
-        var cardTemplate = {
-          'author': {
-            'avatar': 'img/avatars/user0' + (i + 1) + '.png'
-          },
+    for (var i = 0; i < 8; i++) {
+      var cardTemplate = {
+        'author': {
+          'avatar': 'img/avatars/user0' + (i + 1) + '.png'
+        },
 
-          'offer': {
-            'title': CARD_TITLES[i],
-            'address': randomNumber(1000) + ', ' + randomNumber(1000),
-            'price': randomNumber(10000),
-            'type': FLAT_TYPE[randomNumber(3)],
-            'rooms': randomNumber(1, 5),
-            'guests': randomNumber(7),
-            'checkin': generateCheckInOutTime(),
-            'checkout': generateCheckInOutTime(),
-            'features': generateFeatures(featuresTemplate),
-            'description': CARDS_DESCRIPTION[i],
-            'photos': generatePhotos()
-          },
+        'offer': {
+          'title': CARD_TITLES[i],
+          'address': randomNumber(1000) + ', ' + randomNumber(1000),
+          'price': randomNumber(10000),
+          'type': FLAT_TYPE[randomNumber(3)],
+          'rooms': randomNumber(1, 5),
+          'guests': randomNumber(7),
+          'checkin': generateCheckInOutTime(),
+          'checkout': generateCheckInOutTime(),
+          'features': generateFeatures(featuresTemplate),
+          'description': CARDS_DESCRIPTION[i],
+          'photos': generatePhotos()
+        },
 
-          'location': {
-            'x': randomNumber(1, 100),
-            'y': randomNumber(130, 630)
-          }
-        };
-        arr.push(cardTemplate);
-      }
-      return arr;
+        'location': {
+          'x': randomNumber(1, 100),
+          'y': randomNumber(130, 630)
+        }
+      };
+      arr.push(cardTemplate);
     }
+    return arr;
+  };
+
+  window.data = {
+    generateArray: generateArray
   };
 })();
