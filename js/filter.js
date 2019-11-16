7fd840492baf252f128bd871ceacea661582eee6
AlexeySamosadov <asamosadov@gmail.com>
2019-11-16 03:34:08 +0300

    сделал универсальный фильтр

--- js/filter.js
+++ js/filter.js
@@ -20,27 +20,129 @@
   };
 
   var housingTypeValue;
-  var updatePins = function () {
-    activateState(housings);
+  var housingPriceValue;
+  var housingRoomsValue;
+  var housingGuestsValue;
+  var housingWifiValue;
 
-    var filteredHousing = housings.filter(function (it) {
-      return it.offer.type === housingTypeValue;
-    });
-
-    if (housingTypeValue) {
-      activateState(houseSlice(filteredHousing));
-    } else {
-      activateState(houseSlice(housings));
+  var getRang = function (element) {
+    var rang = 0;
+    element.offer.rang = 0;
+    if (element.offer.type === housingTypeValue) {
+      rang += 1;
+      element.offer.rang += 1;
     }
+    if (transferMoney(element.offer.price) === housingPriceValue) {
+      rang += 1;
+      element.offer.rang += 1;
+    }
+    if (element.offer.rooms === housingRoomsValue) {
+      rang += 1;
+      element.offer.rang += 1;
+    }
+    if (element.offer.guests === housingGuestsValue) {
+      rang += 1;
+      element.offer.rang += 1;
+    }
+
+    // var test = function (, ) {
+    //
+    // }
+    var wifi = (element.offer.features)
+      .filter(function (it) {
+        return it === housingWifiValue;
+      });
+
+    if (wifi.length === 1) {
+      rang += 1;
+      element.offer.rang += 1;
+    }
+    return rang;
+  };
+
+  var transferMoney = function (elem) {
+    if (elem < 10000) {
+      return 'low';
+    } else if (elem >= 10000 && elem < 50000) {
+      return 'middle';
+    } else {
+      return 'high';
+    }
+  };
+
+  var updatePins = function () {
+
+    var sortedPins = housings.slice().sort(function (a, b) {
+      var rangDiff = getRang(b) - getRang(a);
+
+      console.log('ранг Дифференc:', rangDiff);
+      return rangDiff;
+    });
+    var maxRang = sortedPins[0].offer.rang;
+
+    var filteredHousing = sortedPins.filter(function (it) {
+      return it.offer.rang === maxRang;
+    });
+    console.log(maxRang);
+    console.log(sortedPins);
+    activateState(filteredHousing);
+
+
+    // var filteredHousing = housings.filter(function (it) {
+    //   if (housingTypeValue) {
+    //     return it.offer.type === housingTypeValue;
+    //   } else if (housingPriceValue) {
+    //     return it.offer.price === housingPriceValue;
+    //   }
+    // });
+
+    // var filteredHousing = housings
+    //   .filter(function (it) {
+    //     return it.offer.type === housingTypeValue;
+    //   })
+    //   .concat(housings.filter(function (it) {
+    //     return it.offer.price === housingPriceValue;
+    //   }));
+
+
+    // if (housingTypeValue) {
+    //   activateState(houseSlice(filteredHousing));
+    // } else {
+    //   activateState(houseSlice(housings));
+    // }
   };
 
   var mapFilter = document.querySelector('.map__filters');
   var housingType = mapFilter.querySelector('#housing-type');
+  var housingPrice = mapFilter.querySelector('#housing-price');
+  var housingRooms = mapFilter.querySelector('#housing-rooms');
+  var housingGuests = mapFilter.querySelector('#housing-guests');
+
+  var housingWifi = mapFilter.querySelector('#filter-wifi');
 
   housingType.addEventListener('change', function () {
     housingTypeValue = housingType.value;
     updatePins();
   });
+  housingPrice.addEventListener('change', function () {
+    housingPriceValue = housingPrice.value;
+    updatePins();
+  });
+  housingRooms.addEventListener('change', function () {
+    housingRoomsValue = +housingRooms.value;
+    updatePins();
+  });
+  housingGuests.addEventListener('change', function () {
+    housingGuestsValue = +housingGuests.value;
+    updatePins();
+  });
+
+  housingWifi.addEventListener('change', function () {
+    housingWifiValue = housingWifi.value;
+    updatePins();
+  });
+
+
 
   window.filter = {
     filteredActivateState: filteredActivateState,
